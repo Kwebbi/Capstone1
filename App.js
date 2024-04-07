bbf00aa8ab446f34397c269215009144099a558c
shajiraguzman <156868034+shajiraguzman@users.noreply.github.com>
2024-04-07 12:07:37 -0500

    moved 'profile' to default when logged in

--- App.js
+++ App.js
@@ -2,7 +2,7 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './components/HomeScreen';
-import AboutScreen from './components/AboutScreen';
+//import AboutScreen from './components/AboutScreen';
 import Register from './components/Register';
 import Login from './components/Login';
 import Profiles from './components/Profiles';
@@ -13,27 +13,27 @@ const Stack = createNativeStackNavigator();
 
 export default function App() {
     const {user} = useAuth();
-    if (user) {
+    if (user) { //these should be screens viewable by only an authorized user
         return (
             <NavigationContainer> 
-                <Stack.Navigator initialRouteName='Home'> 
-                    <Stack.Screen
+                <Stack.Navigator initialRouteName='Profiles'> 
+                    <Stack.Screen           // 'Profiles' is the home screen when a user logs in and is the first screen they see
+                        name="Profiles"
+                        options={{headerShown: false}}
+                        component={Profiles}
+                    />
+                    <Stack.Screen //this screen is currently not in use
                         name="Home"
                         options={{headerShown: false}}
                         component={HomeScreen}
-                    />             
-                    <Stack.Screen
-                        name="About"
-                        options={{headerShown: false}}
-                        component={AboutScreen}
                     />
                 </Stack.Navigator>
             </NavigationContainer>
         );        
     }
     else {
-        return (
-            <NavigationContainer> 
+        return ( //if user is not loggged in, they can only view a login or register screen
+            <NavigationContainer>  
                 <Stack.Navigator initialRouteName='Login'> 
                     <Stack.Screen
                         name="Login"        //initialRouteName is default screen of stack. Login is default 
@@ -41,25 +41,11 @@ export default function App() {
                         component={Login}
                     /> 
                     <Stack.Screen
-                        name="Home"
-                        options={{headerShown: false}}
-                        component={HomeScreen}
-                    />
-                    <Stack.Screen
                         name="Register"
                         options={{headerShown: false}}
                         component={Register}
                     />                
-                    <Stack.Screen
-                        name="About"
-                        options={{headerShown: false}}
-                        component={AboutScreen}
-                    />
-                    <Stack.Screen
-                        name="Profiles"
-                        options={{headerShown: false}}
-                        component={Profiles}
-                    />
+
                 </Stack.Navigator>
             </NavigationContainer>
         );
