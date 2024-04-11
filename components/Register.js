152f90426363b6f1174f2e2427a6350138f9deaa
Kelley Le <kelley.ale11@gmail.com>
2024-04-10 20:54:58 -0500

    Update Register Screen UI

--- components/Register.js
+++ components/Register.js
@@ -5,11 +5,6 @@ import { SafeAreaView, withSafeAreaInsets } from "react-native-safe-area-context
 import { ref, set } from "firebase/database";
 import { auth, database } from '../config/firebase'
 
-
-
-
-
-
 export default function Register({ navigation }) {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -33,7 +28,6 @@ export default function Register({ navigation }) {
     });
   };
 
-
   const handleSubmit = async ()=>{
     if (!isValidPassword) {
       console.log('got error!');
@@ -50,59 +44,53 @@ export default function Register({ navigation }) {
       }
     }
   }
+
   return (
     <ScrollView automaticallyAdjustKeyboardInsets={true}>
-    <View className="flex-1 bg-white" style={{backgroundColor: "#cfe2f3"}}>
-      <SafeAreaView className="flex">
-          <View className="flex-row justify-center">
-            <Image source={require('../assets/logo.png')}
-                style={{width: 200, height: 200}} />
+      <View className="flex-1 bg-white" style={{backgroundColor: "#cfe2f3"}}>
+        <SafeAreaView className="flex">
+            <View className="flex-row justify-center">
+              <Image source={require('../assets/logo.png')}
+                  style={{width: 225, height: 225}} />
+            </View>
+            
+            <View className="flex-row justify-center" style={styles.container}>
+              <Text className="text-white" style={styles.titleText}>Create your account</Text>
+            </View>
+        </SafeAreaView>
+
+        <View className="flex-1 bg-white px-8 pt-8" style={{borderTopLeftRadius: 50, borderTopRightRadius: 50}}>
+          <View className="form space-y-1">
+            <Text className="text-gray-700 ml-2">Full Name</Text>
+            <TextInput className="p-4 bg-gray-100 text-gray-700 rounded-2xl mb-3" 
+              value={fullName} onChangeText={value=> setFullName(value)}placeholder='Enter name'/>
+            
+            <Text className="text-gray-700 ml-2">Email</Text>
+            <TextInput className="p-4 bg-gray-100 text-gray-700 rounded-2xl mb-3" 
+              value={email} onChangeText={value=> setEmail(value)} placeholder='Enter Email'/>
+            
+            <Text className="text-gray-700 ml-2">Password</Text>
+            <TextInput className="p-4 bg-gray-100 text-gray-700 rounded-2xl mb-3" 
+              secureTextEntry value={password} 
+              onChangeText={value=> {
+                setPassword(value);
+                setIsValidPassword(value.length >= 8); }}
+              placeholder='Enter Password'/>
+
+            <TouchableOpacity className="py-3 bg-blue-300 rounded-xl" onPress={handleSubmit}>
+              <Text className="font-xl font-bold text-center text-gray-700">Sign Up</Text>
+            </TouchableOpacity>
+
+            <Text className=""></Text>
+            
+            <View className="flex-row justify-center mb-10"> 
+              <Text className="text-gray-500 font-semibold">Already have an account?</Text>
+              <TouchableOpacity onPress={()=> navigation.navigate('Login')}><Text className="font-semibold text-blue-400"> Login</Text></TouchableOpacity>
+            </View>        
           </View>
-
-          <View className="flex-row justify-center" style={styles.container}>
-            <Text className="text-white" style={styles.titleText}>Welcome!</Text>
-          </View>
-          
-          <View className="flex-row justify-center" style={styles.container}>
-            <Text className="text-white" style={styles.titleText}>Create your account</Text>
-          </View>
-      </SafeAreaView>
-
-      <View className="flex-1 bg-white px-8 pt-8" style={{borderTopLeftRadius: 50, borderTopRightRadius: 50}}>
-      
-        <View className="form space-y-1">
-          <Text className="text-gray-700 ml-4">Full Name</Text>
-          <TextInput className="p-4 bg-gray-100 text-gray-700 rounded-2xl mb-3" 
-            value={fullName} onChangeText={value=> setFullName(value)}placeholder='Enter name'/>
-          
-          <Text className="text-gray-700 ml-4">Email</Text>
-          <TextInput className="p-4 bg-gray-100 text-gray-700 rounded-2xl mb-3" 
-             value={email} onChangeText={value=> setEmail(value)} placeholder='Enter Email'/>
-          
-          <Text className="text-gray-700 ml-4">Password</Text>
-          <TextInput className="p-4 bg-gray-100 text-gray-700 rounded-2xl mb-3" 
-            secureTextEntry value={password} 
-            onChangeText={value=> {
-              setPassword(value);
-              setIsValidPassword(value.length >= 8); }}
-            placeholder='Enter Password'/>
-          
-
-          <TouchableOpacity className="py-3 bg-blue-300 rounded-xl" onPress={handleSubmit}>
-
-            <Text className="font-xl font-bold text-center text-gray-700">Sign Up</Text>
-          </TouchableOpacity>
-          
-          <View className="flex-row justify-center mt-7"> 
-            <Text className="text-gray-500 font-semibold">Already have an account?</Text>
-            <TouchableOpacity onPress={()=> navigation.navigate('Login')}><Text className="font-semibold text-gray-700"> Login</Text></TouchableOpacity>
-          </View>        
         </View>
-       
       </View>
-    </View>
     </ScrollView>
-    
   );
 }
 
@@ -110,7 +98,7 @@ const styles = StyleSheet.create({
 
   titleText: {
     color: '#28436d',
-    fontSize: 20,
+    fontSize: 30,
     fontWeight: 'bold',
   },
 });
