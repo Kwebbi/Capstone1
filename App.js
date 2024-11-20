08b4df9c9b74c575dc7280c5f4a972c7c5d55e9a
Edwin Smith <EdwinSmith@my.unt.edu>
2024-11-20 15:23:26 -0600

    Update App.js

--- App.js
+++ App.js
@@ -10,6 +10,7 @@ import Profiles from "./components/Profiles"
 import ShareBaby from "./components/ShareBaby"
 import ShareRequests from "./components/ShareRequests"
 import BabyMilestones from "./components/BabyMilestones"
+import Comments from "./components/Comments"
 import MilestoneView from "./components/MilestoneView"
 import Settings from "./components/Settings"
 import WeeklyReport from "./components/WeeklyReport"
@@ -19,6 +20,8 @@ import useAuth from "./hooks/useAuth"
 const Stack = createNativeStackNavigator()
 import * as Notifications from "expo-notifications"
 import * as Device from "expo-device"
+import ColorPicker from './components/ColorPicker';
+import { ThemeProvider } from "./components/ThemeContext"; 
 
 export default function App() {
   const { user } = useAuth()
@@ -62,82 +65,96 @@ export default function App() {
 
   if (user) {
     return (
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="Profiles">
-          <Stack.Screen
-            name="Profiles"
-            options={{ headerShown: false }}
-            component={Profiles}
-          />
-          <Stack.Screen
-            name="AddProfile"
-            options={{ headerShown: false }}
-            component={AddProfile}
-          />
-          <Stack.Screen
-            name="HomeScreen"
-            options={{ headerShown: false }}
-            component={HomeScreen}
-          />
-          <Stack.Screen
-            name="ShareBaby"
-            options={{ headerShown: false }}
-            component={ShareBaby}
-          />
-          <Stack.Screen
-            name="ShareRequests"
-            options={{ headerShown: false }}
-            component={ShareRequests}
-          />
-          <Stack.Screen
-            name="BabyMilestones"
-            options={{ headerShown: false }}
-            component={BabyMilestones}
-          />
-          <Stack.Screen
-            name="WeeklyReport"
-            options={{ headerShown: false }}
-            component={WeeklyReport}
-          />
-          <Stack.Screen
-            name="MilestoneView"
-            options={{ headerShown: false }}
-            component={MilestoneView}
-          />
-          <Stack.Screen
-            name="Settings"
-            options={{ headerShown: false, title: "Settings" }}
-            component={Settings}
-          />
-          <Stack.Screen
-            name="EditBaby"
-            options={{ headerShown: false }}
-            component={EditBaby}
-          />
-          <Stack.Screen
-            name="About"
-            options={{ headerShown: false }}
-            component={About}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <ThemeProvider>
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="Profiles">
+            <Stack.Screen
+              name="Profiles"
+              options={{ headerShown: false }}
+              component={Profiles}
+            />
+            <Stack.Screen
+              name="AddProfile"
+              options={{ headerShown: false }}
+              component={AddProfile}
+            />
+            <Stack.Screen
+              name="HomeScreen"
+              options={{ headerShown: false }}
+              component={HomeScreen}
+            />
+            <Stack.Screen
+              name="ShareBaby"
+              options={{ headerShown: false }}
+              component={ShareBaby}
+            />
+            <Stack.Screen
+              name="ShareRequests"
+              options={{ headerShown: false }}
+              component={ShareRequests}
+            />
+            <Stack.Screen
+              name="BabyMilestones"
+              options={{ headerShown: false }}
+              component={BabyMilestones}
+            />
+            <Stack.Screen
+              name="Comments"
+              options={{ headerShown: false }}
+              component={Comments}
+            />
+            <Stack.Screen
+              name="WeeklyReport"
+              options={{ headerShown: false }}
+              component={WeeklyReport}
+            />
+            <Stack.Screen
+              name="MilestoneView"
+              options={{ headerShown: false }}
+              component={MilestoneView}
+            />
+            <Stack.Screen
+              name="Settings"
+              options={{ headerShown: false, title: "Settings" }}
+              component={Settings}
+            />
+            <Stack.Screen
+              name="EditBaby"
+              options={{ headerShown: false }}
+              component={EditBaby}
+            />
+            <Stack.Screen
+              name="About"
+              options={{ headerShown: false }}
+              component={About}
+            />
+            <Stack.Screen
+              name="ColorPicker"
+              options={{ headerShown: true, title: "Pick a Color" }}
+              component={ColorPicker}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </ThemeProvider>
     )
   } else {
     return (
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="Login">
-          <Stack.Screen
-            name="Login" //initialRouteName is default screen of stack. Login is default
-            options={{ headerShown: false }}
-            component={Login}
-          />
-          <Stack.Screen
-            name="Register"
-            options={{ headerShown: false }}
-            component={Register}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <ThemeProvider>
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="Login">
+            <Stack.Screen
+              name="Login" //initialRouteName is default screen of stack. Login is default
+              options={{ headerShown: false }}
+              component={Login}
+            />
+            <Stack.Screen
+              name="Register"
+              options={{ headerShown: false }}
+              component={Register}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </ThemeProvider>
     )
   }
 }
